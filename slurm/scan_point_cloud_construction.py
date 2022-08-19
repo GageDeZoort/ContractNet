d2eac3085db3093eb7b09ab76eab29e2e028d36e
GageDeZoort <jgdezoort@gmail.com>
2022-08-19 13:56:44 -0400

    adding point cloud scanners

---
+++ slurm/scan_point_cloud_construction.py
@@ -0,0 +1,57 @@
+import os
+import sys
+import argparse
+import itertools
+sys.path.append('../')
+sys.path.append('/scratch/gpfs2/jdezoort/oversmoothing/DeepGCNs/')
+
+import numpy as np
+import torch_geometric
+
+from gnn_tracking.preprocessing.point_cloud_builder import PointCloudBuilder
+
+# configure initial params
+idx = int(os.environ['SLURM_ARRAY_TASK_ID'])
+n_evts = 100
+n_sectors = [8, 16, 32, 64, 128, 256]
+sector_di = [-0.0005, -0.00025, -0.0001, 0, 0.0001, 0.00025, 0.0005]
+sector_ds = [0.8, 0.9, 1.0, 1.1, 1.2, 1.3]
+pt_thld = [0.1, 0.5, 0.9]
+params = list(itertools.product(n_sectors, sector_di, sector_ds, pt_thld))
+p = params[idx]
+print(f'n_sectors={p[0]}, sector_di={p[1]}, sector_ds={p[2]}, pt_thld={p[3]}')
+
+# build test graphs
+n_sectors = p[0]
+sector_di = p[1]
+sector_ds = p[2]
+pt_thld = p[3]
+basedir = '/scratch/gpfs/jdezoort/gnn_tracking/'
+pc_builder = PointCloudBuilder(indir=os.path.join(basedir, 'events'),
+                               outdir='../../../studies/ContractNet/temp',
+                               write_output=False,
+                               n_sectors=n_sectors,
+                               sector_di=sector_di, 
+                               sector_ds=sector_ds, thld=p[3], 
+                               pixel_only=True, measurement_mode=True,
+                               remove_noise=False)
+pc_builder.process(n=25, verbose=True)
+
+# extract output measurements
+measurements = pc_builder.get_measurements()
+n_hits = measurements["n_hits"]
+n_hits_err = measurements["n_hits_err"]
+n_hits_ext = measurements["n_hits_ext_err"]
+n_hits_ratio = measurements["n_hits_ratio"]
+n_hits_ratio_err = measurements["n_hits_ratio_err"]
+n_unique_pids = measurements["n_unique_pids"]
+n_unique_pids_err = measurements["n_unique_pids_err"]
+majority_contained = measurements["majority_contained_{str(pt_thld)}"]
+majority_contained_err = measurements["majority_contained_{str(pt_thld)}"]
+
+# write to output
+outfile = os.path.join(basedir, 'studies/ContractNet/slurm/scan_point_cloud_construction.csv')
+with open(outfile, 'a') as f:
+    f.write(f'{n_hits},{n_hits_err},{n_hits_ext},{n_hits_ratio},{n_hits_ratio_err},'
+            +'{n_unique_pids_err},{majority_contained},{majority_contained_err}')
+
